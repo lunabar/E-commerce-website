17f35c44297d84e1103dea22f3429c35d7660ced
lunabar <1434277075@qq.com>
2023-12-31 01:23:03 +0800

    'elementPlus按需自动引入配置'

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 按需引入element-plus需要安装两个插件后导入的配置
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
