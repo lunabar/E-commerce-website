6f2bc79e3188847f2ae59d086c097a772b8a2914
lunabar <1434277075@qq.com>
2024-01-08 22:14:42 +0800

    '二级分类-列表无限加载实现'

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -19,12 +19,12 @@
         categoryId: route.params.id,
         page: 1,
         pageSize: 20,
-        sortField: 'publishTime',
+        sortField: 'publishTime' | 'orderNum' | 'evaluateNum',
     })
 
     const goodList = ref([])
     const getSubCategory = async() => {
-        const res = await getSubCategoryAPI(reqData)
+        const res = await getSubCategoryAPI(reqData.value)
         console.log('二级分类下商品信息的res：',res)
         goodList.value = res.data.result.items
     }
@@ -33,6 +33,26 @@
         getSubCategory()
     })
 
+    const disabled = ref(false)
+    // tab切换回调
+    const tabChange = () => {
+        disabled.value = false
+        reqData.value.page = 1 
+        getSubCategory()
+    }
+
+    
+    const load = async() => {
+        // console.log('加载触底了')
+        reqData.value.page++
+        const res = await getSubCategoryAPI(reqData.value)
+        if(res.data.result.items.length){
+            goodList.value = [...goodList.value, ...res.data.result.items]
+        }else{
+            disabled.value = true
+        }
+        
+    }
 
 
 
@@ -49,12 +69,17 @@
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+        <!-- 点击tab，切换筛选条件参数sortField，重新发送列表请求 -->
+        <!-- v-model='activeName' 绑定的是选中的tab的name值，把reqData的sortField定义为activeName，收集的是商品排序的方式 -->
+        <!-- activeName改变时触发的事件,触发tab-change事件回调tabChange函数 -->
+      <el-tabs v-model="reqData.sortField" @tab-change='tabChange'>
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <!-- 使用elementPlus提供的v-infinite-scroll指令监听是否满足触底条件，满足条件时让页数参数加一获取下一页数据，
+      接着做新老数据的拼接渲染，加载完毕结束监听 -->
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
          <HomeGoodsItem v-for="item in goodList" :key="item.id" :good='item'/>
       </div>
